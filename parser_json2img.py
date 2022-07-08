f009417723b710dca49ce9a9fe0b048b3af7690a
mac <leedongyull@gmail.com>
2022-07-08 20:59:24 +0900

    update code

--- parser_json2img.py
+++ parser_json2img.py
@@ -1,2 +1,25 @@
+import os
+import json
 
-data_path=
+data_path="dataset/dung/train"
+json_path1=os.path.join(data_path,'V-babypoop-9-54.json')
+json_path2=os.path.join(data_path,'V-babypoop-10-169.json')
+dst_path=os.path.join(data_path,'labels')
+
+list_labeled_images = os.listdir(os.path.join(data_path,'labeled_images'))
+
+
+# for img in data_path:
+    # path2img = os.path.join(data_path,img)
+
+with open(json_path2,'r',encoding='utf-8') as f:
+    json_objects=json.load(f)
+
+for obj in json_objects['recog_info']['recog_data']:
+    obj
+
+print(json_objects)
+
+
+
+
